cc6c54a93dcf61bbf98ddf83da2dd2067d0cb930
May Pyone <maypyone015@gmail.com>
2023-12-15 22:58:39 +0630

    Create classes

---
+++ app/models/entity.rb
@@ -0,0 +1,8 @@
+class Entity < ApplicationRecord
+    belongs_to :user
+    belongs_to :group
+  
+    validates :entity_name, presence: true
+    validates :amount, presence: true, numericality: { greater_than: 0 }
+end
+  
