fff9f8592b13cbd5529ae9f489fe2ac6e1ec7039
May Pyone <maypyone015@gmail.com>
2023-12-16 14:39:02 +0630

    Update style

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,3 +1,6 @@
 class SplashController < ApplicationController
-  def index; end
+  skip_before_action :authenticate_user!, only: :index
+  def index
+    
+  end
 end
--- app/models/group.rb
+++ app/models/group.rb
@@ -1,32 +1,31 @@
 class Group < ApplicationRecord
-    belongs_to :user
-    has_many :entities, dependent: :destroy
-    ICONS = {
-      'fa-house' => 'Home',
-      'fa-cart-shopping' => 'Shopping Cart',
-      'fa-car' => 'Car',
-      'fa-truck-medical' => 'Medical Truck',
-      'fa-piggy-bank' => 'Piggy Bank',
-      'fa-credit-card' => 'Credit Card',
-      'fa-globe' => 'World or Global',
-      'fa-briefcase' => 'Business or Work',
-      'fa-film' => 'Movie or Entertainment',
-      'fa-graduation-cap' => 'Education or Learning',
-      'fa-music' => 'Music or Audio',
-      'fa-calendar' => 'Calendar or Events',
-      'fa-wrench' => 'Tools or Settings',
-      'fa-heart' => 'Favorites or Health',
-      'fa-plane' => 'Travel or Flight',
-      'fa-gamepad' => 'Gaming or Entertainment',
-      'fa-microphone' => 'Podcast or Audio',
-      'fa-camera' => 'Photography or Images'
-    }.freeze
-  
-   
-    validates :icon, presence: true, inclusion: { in: ICONS.keys }
-  
-    def total_amount
-      entities.sum(:amount)
-    end
+  belongs_to :user
+  has_many :entities, dependent: :destroy
+  ICONS = {
+    'fa-house' => 'Home',
+    'fa-cart-shopping' => 'Shopping Cart',
+    'fa-car' => 'Car',
+    'fa-truck-medical' => 'Medical Truck',
+    'fa-piggy-bank' => 'Piggy Bank',
+    'fa-credit-card' => 'Credit Card',
+    'fa-globe' => 'World or Global',
+    'fa-briefcase' => 'Business or Work',
+    'fa-film' => 'Movie or Entertainment',
+    'fa-graduation-cap' => 'Education or Learning',
+    'fa-music' => 'Music or Audio',
+    'fa-calendar' => 'Calendar or Events',
+    'fa-wrench' => 'Tools or Settings',
+    'fa-heart' => 'Favorites or Health',
+    'fa-plane' => 'Travel or Flight',
+    'fa-gamepad' => 'Gaming or Entertainment',
+    'fa-microphone' => 'Podcast or Audio',
+    'fa-camera' => 'Photography or Images'
+  }.freeze
+
+
+  validates :icon, presence: true, inclusion: { in: ICONS.keys }
+
+  def total_amount
+    entities.sum(:amount)
+  end
 end
-  
